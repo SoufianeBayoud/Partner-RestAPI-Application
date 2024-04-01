8bb7b386d411e6f52ccb885a35968f32d074b3c0
SoufianeBayoud <soufiane.bayoud@student.ehb.be>
2024-04-01 02:03:15 +0200

    Final Commit

--- src/main/java/com/B2Boost/RestAPIProblem/Repository/PartnerRepository.java
+++ src/main/java/com/B2Boost/RestAPIProblem/Repository/PartnerRepository.java
@@ -2,7 +2,6 @@ package com.B2Boost.RestAPIProblem.Repository;
 
 import com.B2Boost.RestAPIProblem.Model.Partner;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.cdi.JpaRepositoryExtension;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
--- src/test/java/com/B2Boost/RestAPIProblem/Service/PartnerServiceTest.java
+++ src/test/java/com/B2Boost/RestAPIProblem/Service/PartnerServiceTest.java
@@ -20,7 +20,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.times;
 
